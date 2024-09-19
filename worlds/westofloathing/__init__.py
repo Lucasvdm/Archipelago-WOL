b5f49118e093048b3e4645e315542cf48f4449e1
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-09-18 19:03:01 -0600

    Tweaked init, added initial item list

--- worlds/westofloathing/__init__.py
+++ worlds/westofloathing/__init__.py
@@ -1,6 +1,7 @@
+import settings, typing
 from BaseClasses import Item, Location, Tutorial
 from worlds.AutoWorld import WebWorld, World
-from .options import wol_option_groups, wol_option_presets
+from .options import wol_option_groups, wol_option_presets, WOLOptions
 
 class WOLLocation(Location):
     game: str = "West of Loathing"
@@ -24,13 +25,21 @@ class WOLWeb(WebWorld):
     option_groups = wol_option_groups
     options_presets = wol_option_presets
 
+class WOLSettings(settings.Group):
+    """May not need this at all, not sure yet"""
+
 class WOLWorld(World):
     """
     Say howdy to West of Loathing -- a single-player slapstick comedy adventure role-playing game set in the wild west 
     of the Kingdom of Loathing universe. Traverse snake-infested gulches, punch skeletons wearing cowboy hats, grapple 
-    with demon cows, and investigate a wide variety of disgusting spittoons.
-
-    Talk your way out of trouble as a silver-tongued Snake Oiler, plumb the refried mysteries of the cosmos as a wise 
-    and subtle Beanslinger, or let your fists do the talking as a fierce Cow Puncher. Explore a vast open world and 
-    encounter a colorful cast of characters, some of whom are good, many of whom are bad, and a few of whom are ugly.
+    with demon cows, and investigate a wide variety of disgusting spittoons. Explore a vast open world and encounter a 
+    colorful cast of characters, some of whom are good, many of whom are bad, and a few of whom are ugly.
     """
+    game = "West of Loathing"
+    web = WOLWeb()
+
+    options: WOLOptions
+    options_dataclass = WOLOptions
+    #settings: typing.ClassVar[WOLSettings]
+    item_name_groups = {}
+    location_name_groups = {}
