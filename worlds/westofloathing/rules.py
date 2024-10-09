715d0915ead2f5f7430455db7ad1f55eb5212923
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-10-09 16:40:52 -0600

    Added stub rules file, added initial incomplete regions list (sans DLC), tweaked some region names

---
+++ worlds/westofloathing/rules.py
@@ -0,0 +1,11 @@
+from typing import Dict, TYPE_CHECKING
+from worlds.generic.Rules import set_rule, forbid_item, add_rule
+from BaseClasses import CollectionState
+if TYPE_CHECKING:
+    from . import WOLWorld
+
+def set_region_rules(world: "WOLWorld") -> None:
+    """TEMP"""
+
+def set_location_rules(world: "WOLWorld") -> None:
+    """TEMP"""
