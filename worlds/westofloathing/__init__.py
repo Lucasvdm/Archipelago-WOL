ef4b59a2ed4ea9a2f30b555175991639bfdedd59
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-10-23 17:26:01 -0600

    Added create_regions and set_rules in init

--- worlds/westofloathing/__init__.py
+++ worlds/westofloathing/__init__.py
@@ -1,10 +1,12 @@
 import settings
 from typing import List, Dict
-from BaseClasses import Item, Location, Tutorial, ItemClassification
+from BaseClasses import Item, Location, Tutorial, ItemClassification, Region
 from worlds.AutoWorld import WebWorld, World
 from .options import wol_option_groups, wol_option_presets, WOLOptions
 from .items import item_table, item_name_groups, item_name_to_id
 from .locations import location_table, location_name_groups, location_name_to_id
+from .regions import region_table
+from .rules import set_location_rules, set_region_rules
 
 class WOLLocation(Location):
     game: str = "West of Loathing"
@@ -65,3 +67,27 @@ class WOLWorld(World):
                 wol_items.append(self.create_item(item))
 
         self.multiworld.itempool += wol_items
+
+    def create_regions(self) -> None:
+        for region_name in region_table:
+            region = Region(region_name, self.player, self.multiworld)
+            self.multiworld.regions.append(region)
+
+        for region_name, exits in region_table.items():
+            region = self.get_region(region_name)
+            region.add_exits(exits)
+
+        for location_name, location_id in self.location_name_to_id.items():
+            region = self.get_region(location_table[location_name].region)
+            location = WOLLocation(self.player, location_name, location_id, region)
+            region.locations.append(location)
+
+        victory_region = self.get_region("Frisco")
+        victory_location = WOLLocation(self.player, "Temp Victory Location", None, victory_region)
+        victory_location.place_locked_item(WOLItem("Temp Victory Item", ItemClassification.progression, None, self.player))
+        self.multiworld.completion_condition[self.player] = lambda state: state.has("Temp Victory Item", self.player)
+        victory_region.locations.append(victory_location)
+
+    def set_rules(self) -> None:
+        set_region_rules(self)
+        set_location_rules(self)
